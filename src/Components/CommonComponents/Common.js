6c88e4696ee60f14b59df8140aed5c475e6afdeb
harishkanakarajan <60224468+harishkanakarajan@users.noreply.github.com>
2020-01-27 15:52:47 +0530

    MATTR - PIN Generation
    MATTR - PIN Generation - Min length value updated

--- src/Components/CommonComponents/Common.js
+++ src/Components/CommonComponents/Common.js
@@ -1,69 +1,95 @@
-import config from './config'
+import config from "./config";
 
-export const hasValue = (value) => {
-    if (value === "" || value === undefined || value === false) {
-        return false
-    }
-    return true
-}
+export const hasValue = value => {
+  if (value === "" || value === undefined || value === false) {
+    return false;
+  }
+  return true;
+};
 
 // Validate whether 2 digits are been repeated
-export const validateConsecutiveDigits = (num) => {
-    // Breaking the 4 digit pin into array
-    var numArray = num.toString().split('')
+export const validateConsecutiveDigits = num => {
+  // Breaking the 4 digit pin into array
+  var numArray = num.toString().split("");
 
-    for (let i = 0; i < numArray.length - 1; i++) {
-        if (numArray[i] === numArray[i + 1]) {
-            return true
-        } else {
-            continue
-        }
+  for (let i = 0; i < numArray.length - 1; i++) {
+    if (numArray[i] === numArray[i + 1]) {
+      return true;
+    } else {
+      continue;
     }
-    return false
-}
+  }
+  return false;
+};
 
 // Validate value in ASC & DESC order
-export const validateConsecutiveSequence = (num) => {
-    // Breaking the 4 digit pin into array
-    var numArray = num.toString().split('')
+export const validateConsecutiveSequence = num => {
+  // Breaking the 4 digit pin into array
+  var numArray = num.toString().split("");
 
-    var loopIndex = numArray.length % config.consecutiveSequence
+  var loopIndex = numArray.length % config.consecutiveSequence;
 
-    for (let i = 0; i <= loopIndex; i++) {
-        if (parseInt(numArray[i]) + 1 === parseInt(numArray[i + 1]) && parseInt(numArray[i + 1]) + 1 === parseInt(numArray[i + 2])) {
-            return true
-        } else if (parseInt(numArray[i]) - 1 === parseInt(numArray[i + 1]) && parseInt(numArray[i + 1]) - 1 === parseInt(numArray[i + 2])) {
-            return true
-        } else {
-            continue
-        }
+  for (let i = 0; i <= loopIndex; i++) {
+    if (
+      parseInt(numArray[i]) + 1 === parseInt(numArray[i + 1]) &&
+      parseInt(numArray[i + 1]) + 1 === parseInt(numArray[i + 2])
+    ) {
+      return true;
+    } else if (
+      parseInt(numArray[i]) - 1 === parseInt(numArray[i + 1]) &&
+      parseInt(numArray[i + 1]) - 1 === parseInt(numArray[i + 2])
+    ) {
+      return true;
+    } else {
+      continue;
     }
-    return false
-}
+  }
+  return false;
+};
 
 // Validate PIN Already exists
 export const pinAlreadyExists = (arrList, value) => {
-    // Validates whether pin already exists in the saved pin list
-    return arrList.some((row) => row.pin === value)
-}
+  // Validates whether pin already exists in the saved pin list
+  return arrList.some(row => row.pin === value);
+};
+
+export const validatePinLength = num => {
+  // Breaking the pin into array
+  var numArray = num.toString().split("");
+
+  // Validate precedding zeros and total legth
+  if (numArray.length === 3 && numArray[0] !== 0) {
+    numArray.unshift("0");
+  } else if (numArray.length < 3) {
+    return [];
+  }
+  return numArray;
+};
 
 // Prepare valid numbers upfront to generate pin
 export const prepareExcludeNumbers = () => {
-    var validPins = []
+  var validPins = [];
 
-    //Total exclusion between 1010 - 9899 is 2579
-    for (let num = config.minPinValue; num <= config.maxPinValue; num++) {
-
-        // Validate whether 2 digits are been repeated
-        if (validateConsecutiveDigits(num)) {
-            continue
-        } else if (validateConsecutiveSequence(num)) {
-            // Checking consective 3 or more ascending or descending digits in both ASC & DESC order
-            continue
-        } else {
-            validPins.push(num)
-        }
+  //Total exclusion between 1010 - 9899 is 2579
+  for (let num = config.minPinValue; num <= config.maxPinValue; num++) {
+    // Validate PIN Length
+    var pinCheck = validatePinLength(num);
+    if (pinCheck.length > 0) {
+      num = pinCheck.join("");
+    } else {
+      continue;
     }
-    //console.log(validPins) - 6310
-    return validPins
-}
+
+    // Validate whether 2 digits are been repeated
+    if (validateConsecutiveDigits(num)) {
+      continue;
+    } else if (validateConsecutiveSequence(num)) {
+      // Checking consective 3 or more ascending or descending digits in both ASC & DESC order
+      continue;
+    } else {
+      validPins.push(num);
+    }
+  }
+  //console.log(validPins) - 6310
+  return validPins;
+};
--- src/Components/CommonComponents/config.js
+++ src/Components/CommonComponents/config.js
@@ -1,15 +1,15 @@
 const config = {
-    maxLengthPin:  4,
-    inputBoxes: [1,2,3,4,5],
-    minPinValue: 1010, // As we know number starting from 1000 - 1009 has 2 zeros inbetween so excluding it from start number
-    maxPinValue: 9898, // As we know number from 9899 - 9999 has 2 consecutive 9's so excluding it from end index
-    consecutiveSequence: 3,
-    successRes: "success",
-    errorRes: "error",
-    pinSavedSuccess: "PIN saved successfully",
-    pinEmptyMsg: "Invalid/Empty PINS found. Kindly generate and save pin again",
-    pinExistsMsg: "PIN already exists. Kindly regenerate",
-    noPinGenerated: "No Pins generated yet. Kindly generate and save pin",
-}
+  maxLengthPin: 4,
+  inputBoxes: [1, 2, 3, 4, 5],
+  minPinValue: "0000",
+  maxPinValue: "9999",
+  consecutiveSequence: 3,
+  successRes: "success",
+  errorRes: "error",
+  pinSavedSuccess: "PIN saved successfully",
+  pinEmptyMsg: "Invalid/Empty PINS found. Kindly generate and save pin again",
+  pinExistsMsg: "PIN already exists. Kindly regenerate",
+  noPinGenerated: "No Pins generated yet. Kindly generate and save pin"
+};
 
 export default config;
